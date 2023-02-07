0a463eb83db1f2253959f10e9f73c97e6c6106f3
“EdiGordon” <edigordon7@gmail.com  git config --global user.name EdiGordon>
2023-02-07 12:00:43 +0200

    update users

--- dist/routes/users.js
+++ dist/routes/users.js
@@ -37,20 +37,29 @@ router.post("/signup", validateSignUp, userAlreadyExists, (req, res) => __awaite
 router.post("/signin", validateSignIn, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     //email and password:
     try {
-        const user = yield User.findOne({ email: req.body.email });
+        //SELECT * FROM user JOIN Roles ON ...
+        const user = yield User.findOne({ email: req.body.email }).populate("roles");
         if (!user) {
             return res.status(401).json({ message: "No Such User" });
         }
-        //123*12
-        //verify body.password matches user.password
         const isPasswordValid = yield bcrypt.compare(req.body.password, user.password);
         if (!isPasswordValid) {
             return res.status(401).json({ message: "Invalid Credentials" });
         }
-        const token = jwt.sign({ id: user._id }, authConfig.secret, {
+        const token = jwt.sign({ id: user.id }, authConfig.secret, {
             expiresIn: "30d",
         });
-        return res.status(200).json({ message: "Sign in succesfull", token });
+        const authorities = [];
+        for (let i = 0; i < user.roles.length; i++) {
+            authorities.push(`ROLE_` + user.roles[i].name.toUpperCase());
+        }
+        return res.status(200).json({
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            roles: authorities,
+            accessToken: token
+        });
     }
     catch (e) {
         return res.status(500).json({ message: "Server error", error: e });
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -33,14 +33,15 @@ router.post("/signup", validateSignUp, userAlreadyExists, async (req, res) => {
 router.post("/signin", validateSignIn, async (req, res) => {
     //email and password:
     try {
-        const user = await User.findOne({ email: req.body.email });
+        //SELECT * FROM user JOIN Roles ON ...
+        const user = await User.findOne({ email: req.body.email }).populate<{ roles: Array<typeof Role> }>(
+            "roles"
+        );
 
         if (!user) {
             return res.status(401).json({ message: "No Such User" });
         }
 
-        //123*12
-        //verify body.password matches user.password
         const isPasswordValid = await bcrypt.compare(
             req.body.password,
             user.password
@@ -50,10 +51,24 @@ router.post("/signin", validateSignIn, async (req, res) => {
             return res.status(401).json({ message: "Invalid Credentials" });
         }
 
-        const token = jwt.sign({ id: user._id }, authConfig.secret, {
+        const token = jwt.sign({ id: user.id }, authConfig.secret, {
             expiresIn: "30d",
         });
-        return res.status(200).json({ message: "Sign in succesfull", token });
+
+        const authorities = [];
+        for (let i = 0; i < user.roles.length; i++) {
+            authorities.push(`ROLE_` + user.roles[i].name.toUpperCase())
+        }
+
+        return res.status(200).json(
+            {
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                roles: authorities,
+                accessToken: token
+            }
+        );
     } catch (e) {
         return res.status(500).json({ message: "Server error", error: e });
     }
